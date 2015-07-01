ab451b025b094094aacacab738ad0fad8eee5177
Glenn Gillen <me@glenngillen.com>
2015-07-01 12:00:26 +1000

    DRY theme setup.

--- lib/nesta-theme-lisezmoi.rb
+++ lib/nesta-theme-lisezmoi.rb
@@ -12,8 +12,5 @@ end
 asset_path = File.expand_path("../assets", File.dirname(__FILE__))
 view_path = File.expand_path(asset_path + "/views")
 stylesheet_path = File.expand_path(asset_path + "/stylesheets")
-Nesta::ContentFocus::Paths.add_view_path(view_path)
-Nesta::ContentFocus::Paths.add_view_path(File.expand_path(view_path + "/lisezmoi"))
-Nesta::ContentFocus::Paths.add_view_path(stylesheet_path)
-Nesta::ContentFocus::Paths.add_sass_path(stylesheet_path)
+Nesta::Theme.register(:lisezmoi, { views: view_path, styles: stylesheet_path })
 
